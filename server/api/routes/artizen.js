0db73bcf9b88364c190e490a2c2d81083277dcaa
Zian Ke <kezian@umich.edu>
2018-09-19 22:10:22 -0400

    GET artizen relations

--- server/api/routes/artizen.js
+++ server/api/routes/artizen.js
@@ -27,6 +27,94 @@ router.get('/:id', function (req, res, next) {
     });
 });
 
+/* GET artizen relations. */
+router.get('/:id/art', function (req, res, next) {
+    const page = req.query.page >= 0 ? req.query.page : 0;
+    const size = 20;
+
+    // Check artizen exists
+    common.getItem('artizen', req.params.id, function (err, data) {
+        if (err) {
+            next(err);
+        } else {
+            if (data.Count) {
+                // Get art id and type from Aurora table `archive`
+                let sql, parameters;
+                if (req.query.type) {
+                    sql = 'SELECT * FROM archive WHERE artizen_id=? AND type=? ORDER BY art_id DESC LIMIT ? OFFSET ?';
+                    parameters = [parseInt(data.Items[0].id), req.query.type, size, page * size];
+                } else {
+                    sql = 'SELECT * FROM archive WHERE artizen_id=? ORDER BY art_id DESC LIMIT ? OFFSET ?';
+                    parameters = [parseInt(data.Items[0].id), size, page * size];
+                }
+                rds.query(sql, parameters, function (err, result, fields) {
+                    if (result.length) {
+                        // Get art ids and remove duplicate
+                        const art_ids = result.map(item => item.art_id).filter(function (item, index, array) {
+                            return !index || item !== array[index - 1];
+                        }).map(item => ({id: parseInt(item)}));
+
+                        // Get other attributes from DynamoDB table `art`
+                        const params = {
+                            RequestItems: {
+                                'art': {
+                                    Keys: art_ids,
+                                    ProjectionExpression: '#id, #title, #image',
+                                    ExpressionAttributeNames: {
+                                        '#id': 'id',
+                                        '#title': 'title',
+                                        '#image': 'image'
+                                    }
+                                }
+                            },
+                        };
+                        dynamodb.batchGet(params, function (err, data) {
+                            if (err) {
+                                next(err);
+                            } else {
+                                data = data.Responses.art;
+
+                                // Convert data to dict
+                                const art_dict = data.reduce((acc, cur) => {
+                                    acc[cur.id.toString()] = cur;
+                                    return acc;
+                                }, {});
+
+                                // Add DynamoDB data to Aurora results
+                                result = result.map(item => Object.assign({type: item.type}, {data: art_dict[item.art_id.toString()]}));
+
+                                // Group by type
+                                result = result.reduce((acc, cur) => {
+                                    (acc[cur.type] = acc[cur.type] || []).push(cur.data);
+                                    return acc;
+                                }, {});
+
+                                // Convert object to array
+                                result = Object.keys(result).map(function (key) {
+                                    return {
+                                        type: key,
+                                        data: result[key],
+                                        next: `/${req.params.id}/art?type=${key}&page=${page + 1}`
+                                    };
+                                });
+                                res.json(result);
+                            }
+                        });
+                    } else {
+                        // No relations found
+                        res.json([]);
+                    }
+                });
+            } else {
+                res.status(404).json({
+                    code: 'ARTIZEN_NOT_FOUND',
+                    message: `Artizen not found: ${req.params.id}`
+                });
+            }
+        }
+    });
+});
+
 /* PUT artizen username, data. */
 router.put('/:username', function (req, res, next) {
     if (common.validateUsername(req.params.username)) {
