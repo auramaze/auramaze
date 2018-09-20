fb204b3925c8ab8ff43d43979b29a6a8b4aa3a4e
Zian Ke <kezian@umich.edu>
2018-09-19 23:00:32 -0400

    Fix bug in LIMIT and OFFSET of each type

--- server/api/routes/artizen.js
+++ server/api/routes/artizen.js
@@ -29,8 +29,8 @@ router.get('/:id', function (req, res, next) {
 
 /* GET artizen relations. */
 router.get('/:id/art', function (req, res, next) {
-    const page = req.query.page >= 0 ? req.query.page : 0;
-    const size = 20;
+    const page = parseInt(req.query.page) >= 0 ? parseInt(req.query.page) : 0;
+    const size = 2;
 
     // Check artizen exists
     common.getItem('artizen', req.params.id, function (err, data) {
@@ -38,71 +38,82 @@ router.get('/:id/art', function (req, res, next) {
             next(err);
         } else {
             if (data.Count) {
-                // Get art id and type from Aurora table `archive`
-                let sql, parameters;
-                if (req.query.type) {
-                    sql = 'SELECT * FROM archive WHERE artizen_id=? AND type=? ORDER BY art_id DESC LIMIT ? OFFSET ?';
-                    parameters = [parseInt(data.Items[0].id), req.query.type, size, page * size];
-                } else {
-                    sql = 'SELECT * FROM archive WHERE artizen_id=? ORDER BY art_id DESC LIMIT ? OFFSET ?';
-                    parameters = [parseInt(data.Items[0].id), size, page * size];
-                }
-                rds.query(sql, parameters, function (err, result, fields) {
-                    if (result.length) {
-                        // Get art ids and remove duplicate
-                        const art_ids = result.map(item => item.art_id).filter(function (item, index, array) {
-                            return !index || item !== array[index - 1];
-                        }).map(item => ({id: parseInt(item)}));
+                // Get all available types
+                rds.query('SELECT DISTINCT type FROM archive WHERE artizen_id=?', [parseInt(data.Items[0].id)], function (err, result, fields) {
+                    if (err) {
+                        next(err);
+                    } else {
+                        // Get art id and type from Aurora table `archive`
+                        let sql, parameters;
+                        if (req.query.type) {
+                            sql = 'SELECT * FROM archive WHERE artizen_id=? AND type=? ORDER BY art_id DESC LIMIT ? OFFSET ?';
+                            parameters = [parseInt(data.Items[0].id), req.query.type, size, page * size];
+                        } else {
+                            const types = result.map(item => item.type);
+                            sql = Array(types.length).fill('(SELECT * FROM archive WHERE artizen_id=? AND type=? ORDER BY art_id DESC LIMIT ? OFFSET ?)').join(' UNION ALL ') + ' ORDER BY art_id DESC';
+                            parameters = [];
+                            for (let type of types) {
+                                parameters.push(parseInt(data.Items[0].id), type, size, page * size);
+                            }
+                        }
+                        rds.query(sql, parameters, function (err, result, fields) {
+                            if (result.length) {
+                                // Get art ids and remove duplicate
+                                const art_ids = result.map(item => item.art_id).filter(function (item, index, array) {
+                                    return !index || item !== array[index - 1];
+                                }).map(item => ({id: parseInt(item)}));
 
-                        // Get other attributes from DynamoDB table `art`
-                        const params = {
-                            RequestItems: {
-                                'art': {
-                                    Keys: art_ids,
-                                    ProjectionExpression: '#id, #title, #image',
-                                    ExpressionAttributeNames: {
-                                        '#id': 'id',
-                                        '#title': 'title',
-                                        '#image': 'image'
+                                // Get other attributes from DynamoDB table `art`
+                                const params = {
+                                    RequestItems: {
+                                        'art': {
+                                            Keys: art_ids,
+                                            ProjectionExpression: '#id, #title, #image',
+                                            ExpressionAttributeNames: {
+                                                '#id': 'id',
+                                                '#title': 'title',
+                                                '#image': 'image'
+                                            }
+                                        }
+                                    },
+                                };
+                                dynamodb.batchGet(params, function (err, data) {
+                                    if (err) {
+                                        next(err);
+                                    } else {
+                                        data = data.Responses.art;
+
+                                        // Convert data to dict
+                                        const art_dict = data.reduce((acc, cur) => {
+                                            acc[cur.id.toString()] = cur;
+                                            return acc;
+                                        }, {});
+
+                                        // Add DynamoDB data to Aurora results
+                                        result = result.map(item => Object.assign({type: item.type}, {data: art_dict[item.art_id.toString()]}));
+
+                                        // Group by type
+                                        result = result.reduce((acc, cur) => {
+                                            (acc[cur.type] = acc[cur.type] || []).push(cur.data);
+                                            return acc;
+                                        }, {});
+
+                                        // Convert object to array
+                                        result = Object.keys(result).map(function (key) {
+                                            return {
+                                                type: key,
+                                                data: result[key],
+                                                next: `/${req.params.id}/art?type=${key}&page=${page + 1}`
+                                            };
+                                        });
+                                        res.json(result);
                                     }
-                                }
-                            },
-                        };
-                        dynamodb.batchGet(params, function (err, data) {
-                            if (err) {
-                                next(err);
-                            } else {
-                                data = data.Responses.art;
-
-                                // Convert data to dict
-                                const art_dict = data.reduce((acc, cur) => {
-                                    acc[cur.id.toString()] = cur;
-                                    return acc;
-                                }, {});
-
-                                // Add DynamoDB data to Aurora results
-                                result = result.map(item => Object.assign({type: item.type}, {data: art_dict[item.art_id.toString()]}));
-
-                                // Group by type
-                                result = result.reduce((acc, cur) => {
-                                    (acc[cur.type] = acc[cur.type] || []).push(cur.data);
-                                    return acc;
-                                }, {});
-
-                                // Convert object to array
-                                result = Object.keys(result).map(function (key) {
-                                    return {
-                                        type: key,
-                                        data: result[key],
-                                        next: `/${req.params.id}/art?type=${key}&page=${page + 1}`
-                                    };
                                 });
-                                res.json(result);
+                            } else {
+                                // No relations found
+                                res.json([]);
                             }
                         });
-                    } else {
-                        // No relations found
-                        res.json([]);
                     }
                 });
             } else {
