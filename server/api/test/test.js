05cc09172dfc4eeeb65e880b5ea5d0f9f5247e09
Zian Ke <kezian@umich.edu>
2018-09-21 22:28:59 -0400

    Inital tests

--- server/api/test/test.js
+++ server/api/test/test.js
@@ -1,8 +1,29 @@
-var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
+const request = require('supertest');
+const app = require('../app');
+
+describe('Index page', () => {
+    it('should render successfully', done => {
+        request(app).get('/').expect(200, done);
+    });
+});
+
+describe('Art API', () => {
+    describe('GET art data', () => {
+        it('should return data of art', done => {
+            request(app).get('/v1/art/10000002').expect(200)
+                .expect('Content-Type', /json/)
+                .expect(res => res.body.title.en === 'Aristotle with a Bust of Homer')
+                .end(done);
+        });
+        it('should report invalid id', done => {
+            request(app).get('/v1/art/1000002').expect(400)
+                .expect('Content-Type', /json/)
+                .end(done);
+        });
+        it('should report invalid username', done => {
+            request(app).get('/v1/art/as').expect(400)
+                .expect('Content-Type', /json/)
+                .end(done);
         });
     });
 });
