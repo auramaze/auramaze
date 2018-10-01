977ea4efa9114580c3c1f79fa496b700438b20a9
Zian Ke <kezian@umich.edu>
2018-10-01 17:05:57 -0400

    Allow CORS

--- server/api/app.js
+++ server/api/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var searchRouter = require('./routes/search');
@@ -11,6 +12,7 @@ var artizenRouter = require('./routes/artizen');
 var slideRouter = require('./routes/slide');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
