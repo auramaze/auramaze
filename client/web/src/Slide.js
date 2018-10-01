cec7dbf731199f63594a5d5130f2fa4886b1c59b
Zian Ke <kezian@umich.edu>
2018-09-30 21:35:11 -0400

    Toggle reverse animation

--- client/web/src/Slide.js
+++ client/web/src/Slide.js
@@ -6,37 +6,40 @@ class Slide extends Component {
     }
 
     componentWillMount() {
+        const imgStartStyle = (this.props.reverse ? Slide.getImgEndStyle : Slide.getImgStartStyle)(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight);
         this.setState({
             style: {
-                ...Slide.getImgFixedStyle(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight),
-                ...Slide.getImgStartStyle(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight),
+                ...imgStartStyle,
                 opacity: 0
             }
         });
     }
 
     componentDidMount() {
+        const duration = Slide.getImgAnimDuration(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight);
+        const imgEndStyle = (this.props.reverse ? Slide.getImgStartStyle : Slide.getImgEndStyle)(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight);
+
         setTimeout(function () {
             this.setState({
                 style: {
-                    ...Slide.getImgFixedStyle(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight),
-                    ...Slide.getImgEndStyle(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight),
+                    ...imgEndStyle,
                     opacity: 1
                 }
             });
         }.bind(this), 0);
+
         setTimeout(function () {
             this.setState({
                 style: {
-                    ...Slide.getImgFixedStyle(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight),
-                    ...Slide.getImgEndStyle(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight),
+                    ...imgEndStyle,
                     opacity: 0
                 }
             });
-        }.bind(this), Slide.getImgAnimDuration(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight) - 1000);
+        }.bind(this), duration - 1000);
+
         setTimeout(function () {
             this.props.onComplete();
-        }.bind(this), Slide.getImgAnimDuration(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight))
+        }.bind(this), duration);
     }
 
     static getImgFixedStyle(imgWidth, imgHeight, windowWidth, windowHeight) {
@@ -48,29 +51,31 @@ class Slide extends Component {
     }
 
     static getImgStartStyle(imgWidth, imgHeight, windowWidth, windowHeight) {
+        const duration = Slide.getImgAnimDuration(imgWidth, imgHeight, windowWidth, windowHeight);
         if (imgWidth / imgHeight > windowWidth / windowHeight) {
             return {
                 marginLeft: windowWidth - imgWidth * windowHeight / imgHeight,
-                transition: `all ${this.getImgAnimDuration(imgWidth, imgHeight, windowWidth, windowHeight)}ms, opacity 1000ms`
+                transition: `all ${duration}ms, opacity 1000ms`
             };
         } else {
             return {
                 marginTop: windowHeight - imgHeight * windowWidth / imgWidth,
-                transition: `all ${this.getImgAnimDuration(imgWidth, imgHeight, windowWidth, windowHeight)}ms, opacity 1000ms`
+                transition: `all ${duration}ms, opacity 1000ms`
             };
         }
     }
 
     static getImgEndStyle(imgWidth, imgHeight, windowWidth, windowHeight) {
+        const duration = Slide.getImgAnimDuration(imgWidth, imgHeight, windowWidth, windowHeight);
         if (imgWidth / imgHeight > windowWidth / windowHeight) {
             return {
                 marginLeft: 0,
-                transition: `all ${this.getImgAnimDuration(imgWidth, imgHeight, windowWidth, windowHeight)}ms, opacity 1000ms`
+                transition: `all ${duration}ms, opacity 1000ms`
             };
         } else {
             return {
                 marginTop: 0,
-                transition: `all ${this.getImgAnimDuration(imgWidth, imgHeight, windowWidth, windowHeight)}ms, opacity 1000ms`
+                transition: `all ${duration}ms, opacity 1000ms`
             };
         }
     }
@@ -82,14 +87,14 @@ class Slide extends Component {
         } else {
             distance = imgHeight * windowWidth / imgWidth - windowHeight;
         }
-        return Math.round(distance * 20);
+        return Math.max(Math.round(distance * 20), 7000);
     }
 
     render() {
         return (
             <img
                 src={this.props.imgSrc}
-                style={this.state.style}/>
+                style={{...Slide.getImgFixedStyle(this.props.imgWidth, this.props.imgHeight, this.props.windowWidth, this.props.windowHeight), ...this.state.style}}/>
         );
     }
 }
