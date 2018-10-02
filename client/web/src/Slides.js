99bcd8205367529d15b90dab0bb20b499064d4bc
Zian Ke <kezian@umich.edu>
2018-10-01 21:01:23 -0400

    Move static styles to css

--- client/web/src/Slides.js
+++ client/web/src/Slides.js
@@ -80,13 +80,11 @@ class Slides extends Component {
     render() {
         return (
             <div className="Slides"
-                style={{
-                    height: this.state.windowHeight,
-                    width: this.state.windowWidth,
-                    backgroundColor: '#666666',
-                    overflow: 'hidden',
-                }}>
-                {this.state && this.state.hasOwnProperty('index') && this.state[`imgSrc-${this.state.index}`] && this.state[`imgWidth-${this.state.index}`] && this.state[`imgHeight-${this.state.index}`] &&
+                 style={{
+                     height: this.state.windowHeight,
+                     width: this.state.windowWidth,
+                 }}>
+                {(this.state && this.state.hasOwnProperty('index') && this.state[`imgSrc-${this.state.index}`] && this.state[`imgWidth-${this.state.index}`] && this.state[`imgHeight-${this.state.index}`]) &&
                 <Slide
                     key={this.state[`imgSrc-${this.state.index}`]}
                     imgSrc={this.state[`imgSrc-${this.state.index}`]}
