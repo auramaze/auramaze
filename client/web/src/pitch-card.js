70ac89efabdaf8cd4fad0f17e9091d36f90e991e
suwenhao <suwenhao@umich.edu>
2018-10-03 12:25:07 -0400

    Add pitch-card and modify style

---
+++ client/web/src/pitch-card.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import './pitch-card.css';
+import PropTypes from 'prop-types';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+class PitchCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {hovered: false};
+    }
+
+
+    render() {
+        return (
+            <div className='pitch-card'>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td width='60'>
+                            <div className="title">
+                                {this.props.cardTitle}
+                            </div>
+                        </td>
+                        <td width='40'>
+                            <FontAwesomeIcon icon={this.props.cardIconSrc} size="4x"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width='100%'>
+                            <div className="text">
+                                {this.props.cardText}
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+PitchCard.propTypes = {
+    cardTitle: PropTypes.string,
+    cardText: PropTypes.string,
+};
+
+export default PitchCard;
--- client/web/src/pitch.js
+++ client/web/src/pitch.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCamera} from '@fortawesome/free-solid-svg-icons';
+import {faHeadphonesAlt} from '@fortawesome/free-solid-svg-icons';
+import {faShareAlt} from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 import './pitch.css';
+import PitchCard from "./pitch-card";
 
 class Pitch extends Component {
     isWideScreen() {
@@ -13,7 +18,23 @@ class Pitch extends Component {
                 <div className="demo">
                     <img src="https://s3.us-east-2.amazonaws.com/auramaze-test/static/auramaze-demo.png" alt="demo"/>
                 </div>
-                <div className="topics"></div>
+                <div className="topics">
+                    <PitchCard
+                        cardTitle={"VIEW"}
+                        cardText={"Painting identification with camera and image search engine"}
+                        cardIconSrc={faCamera}
+                    />
+                    <PitchCard
+                        cardTitle={"HEAR"}
+                        cardText={"Text-to-speech on introductions via Wikipedia & art experts"}
+                        cardIconSrc={faHeadphonesAlt}
+                    />
+                    <PitchCard
+                        cardTitle={"SHARE"}
+                        cardText={"Platform for every art lover to review favorite artworks"}
+                        cardIconSrc={faShareAlt}
+                    />
+                </div>
             </div>
         );
     }
