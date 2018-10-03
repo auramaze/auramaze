58ce875e18d575b09d4b122519e3cbcbef307f96
kezian <kezian@umich.edu>
2018-10-03 11:54:55 -0400

    Finish pitch layout

--- client/web/src/footer.js
+++ client/web/src/footer.js
@@ -4,27 +4,8 @@ import logo from './logo-white-frame.svg';
 import './footer.css';
 
 class Footer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            wideScreen: document.documentElement.clientWidth > 768
-        };
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
-
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions() {
-        this.setState({
-            wideScreen: document.documentElement.clientWidth > 768
-        });
+    isWideScreen() {
+        return this.props.windowWidth > 768;
     }
 
     render() {
@@ -33,17 +14,17 @@ class Footer extends Component {
                 <table>
                     <tbody>
                         <tr>
-                            {this.state.wideScreen &&
+                            {this.isWideScreen() &&
                         <td width="10%">
                             <img src={logo} className="logo-footer" alt="logo"/>
                         </td>}
-                            <td width={this.state.wideScreen ? '45%' : '80%'}>
+                            <td width={this.isWideScreen() ? '45%' : '80%'}>
                                 <div className="contact-info">
                                 Email: <br/><a href="mailto:service@auramaze.org">service@auramaze.org</a><br/>Address:
                                     <br/>1760 Broadway St.<br/>Ann Arbor, MI 48105
                                 </div>
                             </td>
-                            {this.state.wideScreen &&
+                            {this.isWideScreen() &&
                         <td width="45%">
                             <MapWithAMarker
                                 googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
--- client/web/src/pitch.js
+++ client/web/src/pitch.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import demo from './auramaze-demo.png';
 import './pitch.css';
 
 class Pitch extends Component {
+    isWideScreen() {
+        return this.props.windowWidth > 768;
+    }
+
     render() {
         return (
-            <div>
-                <h2>Pitch</h2>
+            <div className={this.isWideScreen() ? 'pitch' : 'pitch-mobile'}>
+                <div className="demo">
+                    <img src={demo} alt="demo"/>
+                </div>
+                <div className="topics"></div>
             </div>
         );
     }
