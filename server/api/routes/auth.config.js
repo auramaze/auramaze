df82b77c7a964182b36b8a67a593629adae2de64
Zian Ke <kezian@umich.edu>
2018-10-06 18:45:05 -0400

    Config Facebook App id and secret with dotenv

---
+++ server/api/routes/auth.config.js
@@ -0,0 +1,40 @@
+// const providers = ['twitter', 'google', 'facebook', 'github']
+const providers = ['facebook'];
+
+const callbacks = providers.map(provider => {
+    return process.env.NODE_ENV === 'production'
+        ? `https://react-auth-twitter.herokuapp.com/${provider}/callback`
+        : `https://localhost:8080/${provider}/callback`;
+});
+
+// const [twitterURL, googleURL, facebookURL, githubURL] = callbacks
+const [facebookURL] = callbacks;
+
+exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
+    ? 'https://react-auth-twitter.netlify.com'
+    : ['https://127.0.0.1:3000', 'https://localhost:3000'];
+
+// exports.TWITTER_CONFIG = {
+//   consumerKey: process.env.TWITTER_KEY,
+//   consumerSecret: process.env.TWITTER_SECRET,
+//   callbackURL: twitterURL,
+// }
+
+// exports.GOOGLE_CONFIG = {
+//   clientID: process.env.GOOGLE_KEY,
+//   clientSecret: process.env.GOOGLE_SECRET,
+//   callbackURL: googleURL
+// }
+
+exports.FACEBOOK_CONFIG = {
+    clientID: process.env.FACEBOOK_KEY,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    profileFields: ['id', 'emails', 'name', 'picture.width(250)'],
+    callbackURL: facebookURL
+};
+
+// exports.GITHUB_CONFIG = {
+//   clientID: process.env.GITHUB_KEY,
+//   clientSecret: process.env.GITHUB_SECRET,
+//   callbackURL: githubURL
+// }
