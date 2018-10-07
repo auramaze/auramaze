6853c32b7b9051cbb88da9282a934bac83265774
Zian Ke <kezian@umich.edu>
2018-10-07 14:46:09 -0400

    Search both art and artizen

--- server/api/routes/search.js
+++ server/api/routes/search.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 const request = require('request');
 /* GET search results. */
 router.get('/', function (req, res, next) {
-    const keywords = req.query.q;
     let results = {'art': [], 'artizen': []};
 
-
-    request.get({
-        url: 'https://vpc-auramaze-test-lvic4eihmds7zwtnqganecktha.us-east-2.es.amazonaws.com/art/_search',
-        qs: {q: req.query.q},
-        json: true
-    }, (error, response, body) => {
-        if (error){
-            next(error);
-        } else {
-            if (response && response.statusCode === 200) {
-                res.json(body);
-            } else {
-                res.status(response.statusCode);
-            }
-        }
+    const search = _.after(Object.keys(results).length, () => {
+        res.json(results);
     });
+
+    for (let index in results) {
+        request.get({
+            url: `https://vpc-auramaze-test-lvic4eihmds7zwtnqganecktha.us-east-2.es.amazonaws.com/${index}/_search`,
+            qs: {q: req.query.q},
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                next(error);
+            } else {
+                if (response && response.statusCode === 200) {
+                    results[index] = body.hits.hits;
+                    search();
+                } else {
+                    res.status(response.statusCode);
+                }
+            }
+        });
+    }
 });
 
 module.exports = router;
