9601cac51614b757b5b7979d1c9566c9e2c026a6
Zian Ke <kezian@umich.edu>
2018-10-13 16:03:12 -0400

    Init search.js

--- client/web/src/search/search.js
+++ client/web/src/search/search.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import ArtizenCard from '../components/artizen-card';
 import './search.css';
 
 class Search extends Component {
     render() {
         return (
             <div>
-                <h2>Search</h2>
+                <div className="search-result">
+                    <ArtizenCard
+                        style={{width: 250}}
+                        avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
+                        abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
+                    />
+                </div>
             </div>
         );
     }
