87ee48d44447a266784bdbe5444892de1574570e
Zian Ke <kezian@umich.edu>
2018-10-13 13:00:25 -0400

    Validate search query

--- server/api/routes/search.js
+++ server/api/routes/search.js
@@ -3,8 +3,18 @@ const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const request = require('request');
+const {query, validationResult} = require('express-validator/check');
+
 /* GET search results. */
-router.get('/', function (req, res, next) {
+router.get('/', [
+    query('q').exists().isLength({min: 1}),
+    query('from').optional().isInt(),
+], function (req, res, next) {
+    const errors = validationResult(req);
+    if (!validationResult(req).isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
     let results = {'art': [], 'artizen': []};
 
     const search = _.after(Object.keys(results).length, () => {
@@ -14,7 +24,7 @@ router.get('/', function (req, res, next) {
     for (let index in results) {
         request.get({
             url: `${process.env.ESROOT}/${index}/_search`,
-            qs: {q: req.query.q},
+            qs: {q: req.query.q, from: req.query.from || 0, size: 20},
             json: true
         }, (error, response, body) => {
             /* istanbul ignore if */
