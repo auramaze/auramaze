1ffcf21dea9dadd7038229723489ec40e12ecd98
Zian Ke <kezian@umich.edu>
2018-10-13 17:34:43 -0400

    Finish artizen section

--- client/web/src/search/search.js
+++ client/web/src/search/search.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Scrollbars } from 'react-custom-scrollbars';
+import {Scrollbars} from 'react-custom-scrollbars';
 import ArtizenCard from '../components/artizen-card';
 import SectionTitle from '../components/section-title';
 import './search.css';
@@ -11,40 +11,44 @@ class Search extends Component {
                 <div className="result-list">
                     <div className="result-list-container">
                         <SectionTitle sectionTitle="Artizen" style={{margin: '50px 20px 10px 20px'}}/>
-                        <Scrollbars style={{ width: '100%', height: 300 }}>
-                            <div className="result-artizen-container">
-                                <ArtizenCard
-                                    style={{width: 250, display: 'inline-block', margin: 20}}
-                                    avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
-                                    abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
-                                />
-                                <ArtizenCard
-                                    style={{width: 250, display: 'inline-block', margin: 20}}
-                                    avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
-                                    abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
-                                />
-                                <ArtizenCard
-                                    style={{width: 250, display: 'inline-block', margin: 20}}
-                                    avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
-                                    abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
-                                />
-                                <ArtizenCard
-                                    style={{width: 250, display: 'inline-block', margin: 20}}
-                                    avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
-                                    abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
-                                />
-                                <ArtizenCard
-                                    style={{width: 250, display: 'inline-block', margin: 20}}
-                                    avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
-                                    abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
-                                />
-                                <ArtizenCard
-                                    style={{width: 250, display: 'inline-block', margin: 20}}
-                                    avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
-                                    abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
-                                />
-                            </div>
-                        </Scrollbars>
+                        <div className="scrollbar-container" style={{width: '100%', height: 300, position: 'relative'}}>
+                            <Scrollbars style={{width: '100%', height: '100%'}}>
+                                <div className="artizen-container">
+                                    <ArtizenCard
+                                        style={{width: 250, display: 'inline-block', margin: 20}}
+                                        avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
+                                        abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
+                                    />
+                                    <ArtizenCard
+                                        style={{width: 250, display: 'inline-block', margin: 20}}
+                                        avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
+                                        abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
+                                    />
+                                    <ArtizenCard
+                                        style={{width: 250, display: 'inline-block', margin: 20}}
+                                        avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
+                                        abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
+                                    />
+                                    <ArtizenCard
+                                        style={{width: 250, display: 'inline-block', margin: 20}}
+                                        avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
+                                        abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
+                                    />
+                                    <ArtizenCard
+                                        style={{width: 250, display: 'inline-block', margin: 20}}
+                                        avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
+                                        abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
+                                    />
+                                    <ArtizenCard
+                                        style={{width: 250, display: 'inline-block', margin: 20}}
+                                        avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/avatar/paul-cezanne.jpg"
+                                        abstract="Paul Cézanne (US: /seɪˈzæn/ or UK: /sɪˈzæn/; French: [pɔl sezan]; 19 January 1839 – 22 October 1906) was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century. "
+                                    />
+                                </div>
+                            </Scrollbars>
+                            <div className="artizen-container-mask-left"/>
+                            <div className="artizen-container-mask-right"/>
+                        </div>
                     </div>
                 </div>
             </div>
