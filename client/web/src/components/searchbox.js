0fb3247abdbbd175f9c940c46a0a4e7919b89f82
kezian <kezian@umich.edu>
2018-10-14 18:01:19 -0400

    Enable home searchbox

--- client/web/src/components/searchbox.js
+++ client/web/src/components/searchbox.js
@@ -4,13 +4,25 @@ import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import './searchbox.css';
 
 class Searchbox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
     render() {
         return (
             <div {...this.props}>
                 <div className="slogan">AuraMaze</div>
-                <form>
+                <form action="/search" method="get">
                     <div className="searchbox">
-                        <input value="Coming Soon!" readOnly/>
+                        <input name="q" type="text" value={this.state.value} onChange={this.handleChange}/>
                         <div className="icon">
                             <FontAwesomeIcon icon={faSearch} size="lg"/>
                         </div>
