d604b58d64b06c2fb90f1b81bfcfb9c3a600323d
kezian <kezian@umich.edu>
2018-10-16 11:15:39 -0400

    Finish art page

--- client/web/src/art/art.js
+++ client/web/src/art/art.js
@@ -1,12 +1,197 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import request from 'request';
+import SectionTitle from '../components/section-title';
+import ArtizenCard from '../components/artizen-card';
+import TextCard from '../components/text-card';
+import SlickPrevArror from '../components/slick-prev-arrow';
+import SlickNextArror from '../components/slick-next-arrow';
+import {API_ENDPOINT} from '../common';
 import './art.css';
 
 class Art extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            art: {},
+            artizens: [],
+            introductions: [],
+            reviews: []
+        };
+        this.artSection = React.createRef();
+        this.updateArt = this.updateArt.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateArt(this.props.match.params.artId);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.artId !== this.props.match.params.artId) {
+            this.updateArt(this.props.match.params.artId);
+        }
+    }
+
+    updateArt(artId) {
+        this.setState({
+            art: {},
+            artizens: [],
+            introductions: [],
+            reviews: []
+        });
+        request.get({
+            url: `${API_ENDPOINT}/art/${artId}`,
+            json: true
+        }, (error, response, art) => {
+            if (response && response.statusCode === 200) {
+                this.setState({art: art});
+                const id = art.id;
+                if (id) {
+                    request.get({
+                        url: `${API_ENDPOINT}/art/${id}/artizen`,
+                        json: true
+                    }, (error, response, body) => {
+                        if (response && response.statusCode === 200) {
+                            this.setState({artizens: body});
+                        }
+                    });
+
+                    request.get({
+                        url: `${API_ENDPOINT}/art/${id}/introduction`,
+                        json: true
+                    }, (error, response, introductions) => {
+                        if (response && response.statusCode === 200) {
+                            request.post({
+                                url: `${API_ENDPOINT}/artizen/batch`,
+                                body: {id: Array.from(new Set(introductions.map(introduction => introduction.author_id)))},
+                                json: true
+                            }, (error, response, body) => {
+                                if (response && response.statusCode === 200) {
+                                    introductions = introductions.map(introduction =>
+                                        Object.assign(introduction, {
+                                            author_username: body[introduction.author_id].username,
+                                            author_name: body[introduction.author_id].name,
+                                            avatar: body[introduction.author_id].avatar
+                                        }));
+                                    this.setState({introductions: introductions});
+                                }
+                            });
+                        }
+                    });
+
+                    request.get({
+                        url: `${API_ENDPOINT}/art/${id}/review`,
+                        json: true
+                    }, (error, response, reviews) => {
+                        if (response && response.statusCode === 200) {
+                            request.post({
+                                url: `${API_ENDPOINT}/artizen/batch`,
+                                body: {id: Array.from(new Set(reviews.map(review => review.author_id)))},
+                                json: true
+                            }, (error, response, body) => {
+                                if (response && response.statusCode === 200) {
+                                    reviews = reviews.map(review =>
+                                        Object.assign(review, {
+                                            author_username: body[review.author_id].username,
+                                            author_name: body[review.author_id].name,
+                                            avatar: body[review.author_id].avatar
+                                        }));
+                                    this.setState({reviews: reviews});
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    static convertArtizenTypeToSectionTitle(artizenType) {
+        return artizenType.charAt(0).toUpperCase() + artizenType.slice(1);
+    }
+
+    getArtImageWidth() {
+        return this.artSection.current ? this.artSection.current.clientWidth / 2 : 0;
+    }
+
     render() {
         return (
-            <div>
-                <h3>{this.props.match.params.artId}</h3>
+            <div className="art">
+                <div className="art-left-section">
+                    {this.state.artizens.map(section => section.data.length > 0 &&
+                        <div key={section.type}>
+                            <SectionTitle sectionTitle={Art.convertArtizenTypeToSectionTitle(section.type)}/>
+                            {section.data.map(artizen =>
+                                <ArtizenCard
+                                    key={artizen.id}
+                                    id={artizen.id}
+                                    username={artizen.username}
+                                    name={artizen.name && artizen.name.default}
+                                    avatar={artizen.avatar}
+                                    extended={false}
+                                    style={{margin: 20}}
+                                />)}
+                        </div>
+                    )}
+                    <SectionTitle sectionTitle="Reviews"/>
+                    {this.state.reviews.map(review =>
+                        <TextCard
+                            key={review.id}
+                            style={{margin: 20}}
+                            authorId={review.author_id}
+                            authorUsername={review.author_username}
+                            authorName={review.author_name && review.author_name.default}
+                            avatar={review.avatar}
+                            itemType="art"
+                            itemId={review.art_id}
+                            textType="review"
+                            textId={review.id}
+                            rating={review.rating}
+                            content={review.content}
+                            up={review.up}
+                            down={review.down}
+                        />)}
+                </div>
+                <div className="art-right-section" ref={this.artSection}>
+                    <div className="art-title">{this.state.art.title && this.state.art.title.default}</div>
+                    <div className="art-image-container">
+                        <img src={this.state.art.image && this.state.art.image.default.url} alt="image"/>
+                    </div>
+                    <SectionTitle sectionTitle="Introductions"/>
+                    <div className="slider-container">
+                        <Slider
+                            dots
+                            infinite
+                            speed={500}
+                            slidesToShow={1}
+                            slidesToScroll={1}
+                            prevArrow={<SlickPrevArror/>}
+                            nextArrow={<SlickNextArror/>}
+                        >
+                            {this.state.introductions.map(introduction =>
+                                <div className="slide-container" key={introduction.id}>
+                                    <TextCard
+                                        key={introduction.id}
+                                        authorId={introduction.author_id}
+                                        authorUsername={introduction.author_username}
+                                        authorName={introduction.author_name && introduction.author_name.default}
+                                        avatar={introduction.avatar}
+                                        itemType="art"
+                                        itemId={introduction.art_id}
+                                        textType="introduction"
+                                        textId={introduction.id}
+                                        content={introduction.content}
+                                        up={introduction.up}
+                                        down={introduction.down}
+                                    />
+                                </div>)}
+                        </Slider>
+                    </div>
+                </div>
             </div>
         );
     }
