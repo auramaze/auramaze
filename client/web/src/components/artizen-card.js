3bf7447ed37e7da1271768ba830b0e15ad1d25a2
kezian <kezian@umich.edu>
2018-10-17 17:00:48 -0400

    Modify backend architecture demo styles

--- client/web/src/components/artizen-card.js
+++ client/web/src/components/artizen-card.js
@@ -4,16 +4,16 @@ import './artizen-card.css';
 
 class ArtizenCard extends Component {
     render() {
-        const {id, username, name, avatar, abstract, extended, titleFontSize, contentFontSize, ...props} = this.props;
+        const {id, username, name, avatar, abstract, extended, ...props} = this.props;
         return (
             <div {...props} className="artizen-card card-shadow">
-                <Link to={(username || id) ? `/artizen/${username || id}` : '#'}>
+                <Link to={`/artizen/${username || id}`}>
                     <div className="artizen-card-title">
                         {avatar ? <img src={avatar} alt="avatar" className="artizen-card-avatar"/> :
                             <div className="artizen-card-avatar" style={{backgroundColor: '#cdcdcd'}}/>}
-                        <span className="artizen-card-name" style={{fontSize: titleFontSize}}>{name}</span>
+                        <span className="artizen-card-name">{name}</span>
                     </div>
-                    {extended && <div className="artizen-card-abstract" style={{fontSize: contentFontSize}}>
+                    {extended && <div className="artizen-card-abstract">
                         {abstract}
                         <div className="artizen-card-mask-bottom"/>
                     </div>}
--- client/web/src/home/home.js
+++ client/web/src/home/home.js
@@ -5,7 +5,7 @@ import Fade from 'react-reveal/Fade';
 import Slides from './slides';
 import Pitch from './pitch';
 import Footer from '../components/footer';
-import ArtizenCard from '../components/artizen-card';
+import LibraryCard from './library-card';
 import './home.css';
 
 class Home extends Component {
@@ -14,6 +14,7 @@ class Home extends Component {
         this.state = {
             windowWidth: document.documentElement.clientWidth,
             windowHeight: window.innerHeight,
+            clickCount: document.documentElement.clientWidth > 768 ? 0 : 5
         };
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
@@ -39,100 +40,103 @@ class Home extends Component {
             <div className="home">
                 <Slides windowWidth={this.state.windowWidth} windowHeight={this.state.windowHeight}/>
                 <Pitch windowWidth={this.state.windowWidth} windowHeight={this.state.windowHeight}/>
-                <div className="backend-architecture">
-                    <Fade bottom>
-                        <div className="lottie-container">
-                            <Lottie
-                                options={{
-                                    loop: true, autoplay: true, animationData: animationData, renderer: 'svg'
-                                }}
-                            />
-                        </div>
-                    </Fade>
-                </div>
-                <div className="library-cards">
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="Nginx"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_10.png"
-                            abstract="Load balancer to distribute requests to multiple API servers."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
+                <div className={this.state.windowWidth > 768 ? 'backend-architecture' : 'backend-architecture-mobile'}>
+                    <Fade>
+                        <Lottie
+                            options={{
+                                loop: true, autoplay: true, animationData: animationData, renderer: 'svg'
+                            }}
                         />
                     </Fade>
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="Express.js API"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_9.png"
-                            abstract="Provide endpoints for GET, PUT, POST, DELETE actions of artwork information. Connected with Aurora and DynamoDB to update databases."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
-                        /></Fade>
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="AWS Aurora"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_5.png"
-                            abstract="Basic information of artworks, artist, museums, genres, etc. and users, as well as relationship between items including painting, collecting, exhibiting, etc."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
-                        /></Fade>
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="AWS DynamoDB"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_4.png"
-                            abstract="Supplementary data of items, such as museum address, artist’s date of birth. These data are all key-value pairs fetched from public domain sources (i.e. Wikipedia) by web crawlers."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
-                        /></Fade>
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="AWS S3"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_3.png"
-                            abstract="Raw image of artworks and static image content of the website."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
-                        /></Fade>
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="Kafka"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_2.png"
-                            abstract="Message queue and CDC tool to capture database change and update Microservices in real time."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
-                        /></Fade>
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="ElasticSearch"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_7.png"
-                            abstract="Text Search Engine and Image Signature Search Engine."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
-                        /></Fade>
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="Tensorflow"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_6.png"
-                            abstract="Machine learning model for Recommender System."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
-                        /></Fade>
-                    <Fade bottom>
-                        <ArtizenCard
-                            style={{margin: 20}}
-                            name="Stanford CoreNLP"
-                            avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_8.png"
-                            abstract="Natural Language Processing on user input comments to analyze preference on art."
-                            extended titleFontSize={25}
-                            contentFontSize={20}
-                        /></Fade>
+                </div>
+                <div
+                    className={this.state.windowWidth > 768 ? 'library-cards' : 'library-cards-mobile'}
+                    style={{cursor: this.state.clickCount < 5 ? 'pointer' : 'default'}}
+                    onClick={() => {
+                        this.setState({clickCount: this.state.clickCount + 1});
+                    }}
+                >
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 0}>
+                            <LibraryCard
+                                name="Nginx"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_21.png"
+                                content="Load balancer to redirect requests to multiple API servers."
+                            />
+                        </Fade>
+                    </div>
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 1}>
+                            <LibraryCard
+                                name="Express.js"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_9.png"
+                                content="Provide API endpoints for GET, PUT, POST, DELETE actions of artwork information. Connected with Aurora and DynamoDB to update databases."
+                            />
+                        </Fade>
+                    </div>
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 2}>
+                            <LibraryCard
+                                name="Amazon Aurora"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_5.png"
+                                content="Basic information of artworks, artist, museums, genres, etc. and users, as well as relationship between items including painting, collecting, exhibiting, etc."
+                            />
+                        </Fade>
+                    </div>
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 2}>
+                            <LibraryCard
+                                name="Amazon DynamoDB"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_4.png"
+                                content="Supplementary data of items, such as museum address, artist’s date of birth. These data are all key-value pairs fetched from public domain sources (i.e. Wikipedia) by web crawlers."
+                            />
+                        </Fade>
+                    </div>
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 2}>
+                            <LibraryCard
+                                name="Amazon S3"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_3.png"
+                                content="Raw image of artworks and static image content of the website."
+                            />
+                        </Fade>
+                    </div>
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 3}>
+                            <LibraryCard
+                                name="Kafka"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_20.png"
+                                content="Message queue and CDC tool to capture database change and update Microservices in real time."
+                            />
+                        </Fade>
+                    </div>
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 4}>
+                            <LibraryCard
+                                name="ElasticSearch"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_7.png"
+                                content="Text Search Engine and Image Signature Search Engine."
+                            />
+                        </Fade>
+                    </div>
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 4}>
+                            <LibraryCard
+                                name="TensorFlow"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_6.png"
+                                content="Machine learning model for Recommender System."
+                            />
+                        </Fade>
+                    </div>
+                    <div className="library-card-item">
+                        <Fade bottom when={this.state.clickCount > 4}>
+                            <LibraryCard
+                                name="Stanford CoreNLP"
+                                avatar="https://s3.us-east-2.amazonaws.com/auramaze-test/architecture/img_8.png"
+                                content="Natural Language Processing on user input comments to analyze preference on art."
+                            />
+                        </Fade>
+                    </div>
                 </div>
                 <Footer windowWidth={this.state.windowWidth} windowHeight={this.state.windowHeight}/>
             </div>
