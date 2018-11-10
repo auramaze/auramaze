6910bedb84ea208a84e9bb5259b7754049aee866
Zian Ke <kezian@umich.edu>
2018-11-10 14:34:09 -0500

    Upsert id into database

--- server/api/routes/auth.callback.js
+++ server/api/routes/auth.callback.js
@@ -1,15 +1,45 @@
+const common = require('./common');
+const rds = common.rds;
+
 exports.google = (accessToken, refreshToken, profile, cb) => {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(JSON.stringify(profile));
-    profile.auramaze_id = 'auramaze';
-    return cb(null, profile);
+    rds.query('INSERT INTO artizen (google, name) VALUES ? ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)',
+        [[profile.id, JSON.stringify({default: profile.displayName})]],
+        (err, result, fields) => {
+            /* istanbul ignore if */
+            if (err) {
+                return cb(null, false);
+            } else {
+                rds.query('SELECT LAST_INSERT_ID() AS id', (err, result, fields) => {
+                    /* istanbul ignore if */
+                    if (err) {
+                        return cb(null, false);
+                    } else {
+                        profile.auramaze_id = result[0].id;
+                        return cb(null, profile);
+                    }
+                });
+            }
+        });
 };
 
 exports.facebook = (accessToken, refreshToken, profile, cb) => {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(JSON.stringify(profile));
-    profile.auramaze_id = 'auramaze';
-    return cb(null, profile);
+    const {givenName, familyName} = profile.name;
+    rds.query('INSERT INTO artizen (facebook, name) VALUES ? ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)',
+        [[profile.id, JSON.stringify({default: `${givenName} ${familyName}`})]],
+        (err, result, fields) => {
+            /* istanbul ignore if */
+            if (err) {
+                return cb(null, false);
+            } else {
+                rds.query('SELECT LAST_INSERT_ID() AS id', (err, result, fields) => {
+                    /* istanbul ignore if */
+                    if (err) {
+                        return cb(null, false);
+                    } else {
+                        profile.auramaze_id = result[0].id;
+                        return cb(null, profile);
+                    }
+                });
+            }
+        });
 };
