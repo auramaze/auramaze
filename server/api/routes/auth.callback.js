93f558ce0835bfe1965cc5f215fc113861e06d99
Zian Ke <kezian@umich.edu>
2018-11-10 14:20:06 -0500

    Fix facebook callback

--- server/api/routes/auth.callback.js
+++ server/api/routes/auth.callback.js
@@ -6,14 +6,10 @@ exports.google = (accessToken, refreshToken, profile, cb) => {
     return cb(null, profile);
 };
 
-exports.facebook = (req, res) => {
-    const io = req.app.get('io');
-    const {givenName, familyName} = req.user.name;
-    console.log(JSON.stringify(req.user));
-    const user = {
-        name: `${givenName} ${familyName}`,
-        photo: req.user.photos[0].value
-    };
-    io.in(req.session.socketId).emit('facebook', user);
-    res.end();
+exports.facebook = (accessToken, refreshToken, profile, cb) => {
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(JSON.stringify(profile));
+    profile.auramaze_id = 'auramaze';
+    return cb(null, profile);
 };
