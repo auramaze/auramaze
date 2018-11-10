e9abe5ee53066b1a0a08e2f081929bd918cf132d
Zian Ke <kezian@umich.edu>
2018-11-10 14:08:25 -0500

    Test setting auramaze id

--- server/api/routes/auth.controller.js
+++ server/api/routes/auth.controller.js
@@ -21,6 +21,7 @@ exports.google = (req, res) => {
 exports.facebook = (req, res) => {
     const io = req.app.get('io');
     const {givenName, familyName} = req.user.name;
+    console.log(JSON.stringify(req.user));
     const user = {
         name: `${givenName} ${familyName}`,
         photo: req.user.photos[0].value
--- server/api/routes/passport.init.js
+++ server/api/routes/passport.init.js
@@ -33,6 +33,7 @@ module.exports = () => {
         console.log(accessToken);
         console.log(refreshToken);
         console.log(JSON.stringify(profile));
+        profile.auramaze_id = 'auramaze';
         return cb(null, profile);
     };
 
