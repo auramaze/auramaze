3e69e74d3dc9c2df81f0c0007ebda0f77732cae4
Zian Ke <kezian@umich.edu>
2018-11-12 16:31:17 -0500

    Use WindowContext in navbar-mobile

--- client/web/src/components/navbar-mobile.js
+++ client/web/src/components/navbar-mobile.js
@@ -5,7 +5,7 @@ import {HashLink} from 'react-router-hash-link';
 import * as Scroll from 'react-scroll';
 import logo from '../static/logo-white-frame.svg';
 import './navbar-mobile.css';
-import {ModalContext} from '../app';
+import {ModalContext, WindowContext} from '../app';
 import {lockBody, unlockBody} from '../utils';
 import {withCookies} from "react-cookie";
 import {removeCookies} from "../utils";
@@ -16,28 +16,9 @@ class NavbarMobile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            windowWidth: document.documentElement.clientWidth,
-            windowHeight: document.documentElement.clientHeight,
             expand: false
         };
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
         this.hideNavbarMobile = this.hideNavbarMobile.bind(this);
-    }
-
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions() {
-        this.setState({
-            windowWidth: document.documentElement.clientWidth,
-            windowHeight: document.documentElement.clientHeight
-        });
     }
 
     hideNavbarMobile() {
@@ -51,82 +32,87 @@ class NavbarMobile extends Component {
         const id = cookies.get('id');
         const username = cookies.get('username');
         return (
-            <ModalContext.Consumer>
-                {({showSignupModal, showLoginModal}) => (
-                    <div className="navbar-mobile" style={{backgroundColor: home ? '' : '#000000'}}>
-                        <div
-                            className={`nav-items-mobile ${!this.state.expand && 'nav-items-mobile-collapse'}`}
-                            style={{width: this.state.windowWidth, height: this.state.windowHeight}}
-                        >
-                            <div className="nav-items-wrapper-mobile">
-                                <div className="nav-item-mobile">
-                                    {home ?
-                                        <Link to="#" onClick={(e) => {
-                                            e.preventDefault();
-                                            this.hideNavbarMobile();
-                                            scroll.scrollTo(document.documentElement.clientHeight);
-                                        }}>About</Link> :
-                                        <HashLink to="/#about" onClick={this.hideNavbarMobile}>About</HashLink>}
+            <WindowContext.Consumer>
+                {({windowWidth, windowHeight}) => (
+                    <ModalContext.Consumer>
+                        {({showSignupModal, showLoginModal}) => (
+                            <div className="navbar-mobile" style={{backgroundColor: home ? '' : '#000000'}}>
+                                <div
+                                    className={`nav-items-mobile ${!this.state.expand && 'nav-items-mobile-collapse'}`}
+                                    style={{width: windowWidth, height: windowHeight}}
+                                >
+                                    <div className="nav-items-wrapper-mobile">
+                                        <div className="nav-item-mobile">
+                                            {home ?
+                                                <Link to="#" onClick={(e) => {
+                                                    e.preventDefault();
+                                                    this.hideNavbarMobile();
+                                                    scroll.scrollTo(windowHeight);
+                                                }}>About</Link> :
+                                                <HashLink to="/#about" onClick={this.hideNavbarMobile}>About</HashLink>}
+                                        </div>
+                                        <div className="nav-item-mobile">
+                                            {home ?
+                                                <Link to="#" onClick={(e) => {
+                                                    e.preventDefault();
+                                                    this.hideNavbarMobile();
+                                                    scroll.scrollToBottom();
+                                                }}>Contact</Link> :
+                                                <HashLink to="/#contact"
+                                                          onClick={this.hideNavbarMobile}>Contact</HashLink>}
+                                        </div>
+                                        {!id && <div className="nav-item-mobile">
+                                            <Link to="#" onClick={(e) => {
+                                                e.preventDefault();
+                                                this.hideNavbarMobile();
+                                                showSignupModal();
+                                            }}>Sign up</Link>
+                                        </div>}
+                                        {!id && <div className="nav-item-mobile">
+                                            <Link to="#" onClick={(e) => {
+                                                e.preventDefault();
+                                                this.hideNavbarMobile();
+                                                showLoginModal();
+                                            }}>Log in</Link>
+                                        </div>}
+                                        {id && <div className="nav-item-mobile">
+                                            <Link
+                                                to={`/artizen/${username || id}`}
+                                                onClick={this.hideNavbarMobile}>{username || id}
+                                            </Link>
+                                        </div>}
+                                        {id && <div className="nav-item-mobile">
+                                            <Link to="#" onClick={(e) => {
+                                                e.preventDefault();
+                                                this.hideNavbarMobile();
+                                                removeCookies(cookies);
+                                                window.location.reload();
+                                            }}>Log out</Link>
+                                        </div>}
+                                    </div>
                                 </div>
-                                <div className="nav-item-mobile">
-                                    {home ?
-                                        <Link to="#" onClick={(e) => {
-                                            e.preventDefault();
-                                            this.hideNavbarMobile();
-                                            scroll.scrollToBottom();
-                                        }}>Contact</Link> :
-                                        <HashLink to="/#contact" onClick={this.hideNavbarMobile}>Contact</HashLink>}
-                                </div>
-                                {!id && <div className="nav-item-mobile">
-                                    <Link to="#" onClick={(e) => {
-                                        e.preventDefault();
-                                        this.hideNavbarMobile();
-                                        showSignupModal();
-                                    }}>Sign up</Link>
-                                </div>}
-                                {!id && <div className="nav-item-mobile">
-                                    <Link to="#" onClick={(e) => {
-                                        e.preventDefault();
-                                        this.hideNavbarMobile();
-                                        showLoginModal();
-                                    }}>Log in</Link>
-                                </div>}
-                                {id && <div className="nav-item-mobile">
-                                    <Link
-                                        to={`/artizen/${username || id}`}
-                                        onClick={this.hideNavbarMobile}>{username || id}
+                                <div className="nav-logo-mobile">
+                                    <Link to="/">
+                                        <img src={logo} className="logo-mobile" alt="logo"/>
                                     </Link>
-                                </div>}
-                                {id && <div className="nav-item-mobile">
-                                    <Link to="#" onClick={(e) => {
-                                        e.preventDefault();
-                                        this.hideNavbarMobile();
-                                        removeCookies(cookies);
-                                        window.location.reload();
-                                    }}>Log out</Link>
-                                </div>}
+                                </div>
+                                <div className={`nav-toggle ${this.state.expand && 'nav-toggle-cancel'}`}
+                                     onClick={() => {
+                                         if (this.state.expand) {
+                                             unlockBody();
+                                         } else {
+                                             lockBody();
+                                         }
+                                         this.setState({expand: !this.state.expand});
+                                     }}>
+                                    <div className="nav-toggle-line" id="nav-toggle-1"/>
+                                    <div className="nav-toggle-line" id="nav-toggle-2"/>
+                                    <div className="nav-toggle-line" id="nav-toggle-3"/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="nav-logo-mobile">
-                            <Link to="/">
-                                <img src={logo} className="logo-mobile" alt="logo"/>
-                            </Link>
-                        </div>
-                        <div className={`nav-toggle ${this.state.expand && 'nav-toggle-cancel'}`} onClick={() => {
-                            if (this.state.expand) {
-                                unlockBody();
-                            } else {
-                                lockBody();
-                            }
-                            this.setState({expand: !this.state.expand});
-                        }}>
-                            <div className="nav-toggle-line" id="nav-toggle-1"/>
-                            <div className="nav-toggle-line" id="nav-toggle-2"/>
-                            <div className="nav-toggle-line" id="nav-toggle-3"/>
-                        </div>
-                    </div>
-                )}
-            </ModalContext.Consumer>
+                        )}
+                    </ModalContext.Consumer>)}
+            </WindowContext.Consumer>
         );
     }
 }
