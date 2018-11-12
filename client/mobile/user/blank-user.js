cd59e5b4cfe5f3d9706400e83655840d75dc3d69
suwenhao <suwenhao@umich.edu>
2018-11-11 22:31:45 -0500

    Add login page and sign up page

--- client/mobile/user/blank-user.js
+++ client/mobile/user/blank-user.js
@@ -15,6 +15,7 @@ import google from '../icons/google.png';
 import facebook from '../icons/facebook.png';
 import {Input} from "react-native-elements";
 import Hr from 'react-native-hr-plus';
+import SignUpPage from "./sign-up-page";
 
 const DismissKeyboard = ({children}) => (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -83,62 +84,6 @@ class BlankUser extends React.Component {
             mainStruct: {
                 flex: 1, flexDirection: 'column',
                 alignItems: 'center'
-            },
-            textWithDivider: {
-                color: '#666666',
-                paddingHorizontal: 10
-            },
-            buttonGeneral: {
-                flexDirection: 'row', alignItems: 'center',
-                justifyContent: 'center',
-                width: Dimensions.get('window').width - 40,
-                height: 45,
-                marginVertical: 10,
-                borderWidth: 1
-                // borderRadius: 3
-            },
-            buttonAuramaze: {
-                backgroundColor: '#666666',
-                borderColor: '#666666'
-            },
-            buttonGoogle: {
-                backgroundColor: 'white',
-                borderColor: '#666666'
-            },
-            buttonFacebook: {
-                backgroundColor: '#3B5998',
-                borderColor: '#3B5998'
-            },
-            signupScreenButton: {
-                width: Dimensions.get('window').width * 2 / 3,
-                marginRight: 40,
-                marginLeft: 40,
-                paddingTop: 10,
-                paddingBottom: 10,
-                backgroundColor: 'white',
-                // borderWidth: 1,
-                borderColor: '#666666',
-                borderRadius: 5
-            },
-            textGenreal: {
-                textAlign: 'center',
-                paddingHorizontal: 10,
-                fontSize: 15
-            },
-            textAuramaze: {
-                color: 'white'
-            },
-            textGoogle: {
-                color: 'black'
-            },
-            textFacebook: {
-                color: 'white'
-            },
-            signupText: {
-                color: '#666666',
-                textAlign: 'center',
-                paddingHorizontal: 10,
-                fontSize: 15
             }
         });
 
@@ -151,60 +96,7 @@ class BlankUser extends React.Component {
                                      style={{marginTop: 80, marginBottom: 30}}/>
 
 
-                    <Input placeholder='Name'
-                           inputContainerStyle={{borderBottomColor: '#cdcdcd'}}
-                           onChangeText={(username) => this.setState(previousState => ({name: username}))}
-                    />
-                    <Input placeholder='Email'
-                           inputContainerStyle={{borderBottomColor: '#cdcdcd'}}
-                           onChangeText={(email) => this.setState(previousState => ({email: email}))}
-                    />
-                    <Input placeholder='Password'
-                           inputContainerStyle={{borderBottomColor: '#cdcdcd'}}
-                           onChangeText={(password) => this.setState(previousState => ({password: password}))}
-                           containerStyle={{marginBottom: 10}}
-                    />
-
-                    <TouchableOpacity
-                        style={[styles.buttonGeneral, styles.buttonAuramaze]}
-                        onPress={this.createAuraMaze}
-                        underlayColor='#fff'>
-                        <AutoHeightImage
-                            width={20}
-                            source={logoIcon} style={{tintColor: 'white'}}/>
-                        <Text style={[styles.textGenreal, styles.textAuramaze]}>Create AuraMaze account</Text>
-                    </TouchableOpacity>
-
-                    <Hr color='#666666' width={1} style={{paddingHorizontal: 20}}>
-                        <Text style={styles.textWithDivider}>OR</Text>
-                    </Hr>
-
-                    <TouchableOpacity
-                        style={[styles.buttonGeneral, styles.buttonGoogle]}
-                        onPress={BlankUser._retrieveData}
-                        underlayColor='#fff'>
-                        <AutoHeightImage
-                            width={20}
-                            source={google}/>
-                        <Text style={[styles.textGenreal, styles.textGoogle]}>Sign up with Google</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={[styles.buttonGeneral, styles.buttonFacebook]}
-                        onPress={BlankUser._onPressButton}
-                        underlayColor='#fff'>
-                        <AutoHeightImage
-                            width={20}
-                            source={facebook}/>
-                        <Text style={[styles.textGenreal, styles.textFacebook]}>Sign up with Facebook</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={styles.signupScreenButton}
-                        onPress={BlankUser._onPressButton}
-                        underlayColor='#fff'>
-                        <Text style={styles.signupText}>Already have an account? Log In</Text>
-                    </TouchableOpacity>
+                    <SignUpPage/>
 
                 </View>
             </DismissKeyboard>
---
+++ client/mobile/user/sign-up-page.js
@@ -0,0 +1,206 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Dimensions,
+    TouchableWithoutFeedback,
+    Text,
+    TouchableOpacity,
+    Keyboard,
+    AsyncStorage
+} from 'react-native';
+import AutoHeightImage from "react-native-auto-height-image";
+import logoIcon from "../assets/auramaze-logo.png";
+import google from '../icons/google.png';
+import facebook from '../icons/facebook.png';
+import {Input} from "react-native-elements";
+import Hr from 'react-native-hr-plus';
+
+class SignUpPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.createAuraMaze = this.createAuraMaze.bind(this);
+    }
+
+    static _onPressButton() {
+        alert("asd");
+    };
+
+    createAuraMaze() {
+        this.setState(previousState => ({auramazeProcessing: true}));
+        let bodyObject = JSON.stringify({
+            name: {default: this.state.name},
+            email: this.state.email,
+            password: this.state.password
+        });
+        fetch('https://apidev.auramaze.org/v1/auth/signup', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json"
+            },
+            body: bodyObject
+        }).then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Create account fail.');
+            }
+        }).then(function (myJson) {
+            alert(JSON.stringify(myJson));
+            this.setState(previousState => ({auramazeProcessing: false}));
+        }).catch(function (error) {
+            this.setState(previousState => ({auramazeProcessing: false}));
+            alert('There has been a problem with your fetch operation: ' + error.message);
+        });
+    };
+
+    static _retrieveData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('key');
+            if (value !== null) {
+                // We have data!!
+                alert(value);
+            }
+        } catch (error) {
+            // Error retrieving data
+        }
+    };
+
+    render() {
+
+        const styles = StyleSheet.create({
+            mainStruct: {
+                flex: 1, flexDirection: 'column',
+                alignItems: 'center'
+            },
+            textWithDivider: {
+                color: '#666666',
+                paddingHorizontal: 10
+            },
+            buttonGeneral: {
+                flexDirection: 'row', alignItems: 'center',
+                justifyContent: 'center',
+                width: Dimensions.get('window').width - 40,
+                height: 45,
+                marginVertical: 10,
+                borderWidth: 1
+                // borderRadius: 3
+            },
+            buttonAuramaze: {
+                backgroundColor: '#666666',
+                borderColor: '#666666'
+            },
+            buttonGoogle: {
+                backgroundColor: 'white',
+                borderColor: '#666666'
+            },
+            buttonFacebook: {
+                backgroundColor: '#3B5998',
+                borderColor: '#3B5998'
+            },
+            signupScreenButton: {
+                width: Dimensions.get('window').width * 2 / 3,
+                marginRight: 40,
+                marginLeft: 40,
+                paddingTop: 10,
+                paddingBottom: 10,
+                backgroundColor: 'white',
+                // borderWidth: 1,
+                borderColor: '#666666',
+                borderRadius: 5
+            },
+            textGenreal: {
+                textAlign: 'center',
+                paddingHorizontal: 10,
+                fontSize: 15
+            },
+            textAuramaze: {
+                color: 'white'
+            },
+            textGoogle: {
+                color: 'black'
+            },
+            textFacebook: {
+                color: 'white'
+            },
+            signupText: {
+                color: '#666666',
+                textAlign: 'center',
+                paddingHorizontal: 10,
+                fontSize: 15
+            }
+        });
+
+        return (
+            <View>
+                <View style={{
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+
+                    <Input placeholder='Name'
+                           inputContainerStyle={{borderBottomColor: '#cdcdcd'}}
+                           onChangeText={(username) => this.setState(previousState => ({name: username}))}
+                    />
+                    <Input placeholder='Email'
+                           inputContainerStyle={{borderBottomColor: '#cdcdcd'}}
+                           onChangeText={(email) => this.setState(previousState => ({email: email}))}
+                    />
+                    <Input placeholder='Password'
+                           inputContainerStyle={{borderBottomColor: '#cdcdcd'}}
+                           onChangeText={(password) => this.setState(previousState => ({password: password}))}
+                           containerStyle={{marginBottom: 10}}
+                    />
+                </View>
+
+                <TouchableOpacity
+                    style={[styles.buttonGeneral, styles.buttonAuramaze]}
+                    onPress={this.createAuraMaze}
+                    underlayColor='#fff'>
+                    <AutoHeightImage
+                        width={20}
+                        source={logoIcon} style={{tintColor: 'white'}}/>
+                    <Text style={[styles.textGenreal, styles.textAuramaze]}>Create AuraMaze account</Text>
+                </TouchableOpacity>
+
+                <Hr color='#666666' width={1} style={{paddingHorizontal: 20}}>
+                    <Text style={styles.textWithDivider}>OR</Text>
+                </Hr>
+
+                <TouchableOpacity
+                    style={[styles.buttonGeneral, styles.buttonGoogle]}
+                    onPress={SignUpPage._retrieveData}
+                    underlayColor='#fff'>
+                    <AutoHeightImage
+                        width={20}
+                        source={google}/>
+                    <Text style={[styles.textGenreal, styles.textGoogle]}>Sign up with Google</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={[styles.buttonGeneral, styles.buttonFacebook]}
+                    onPress={SignUpPage._onPressButton}
+                    underlayColor='#fff'>
+                    <AutoHeightImage
+                        width={20}
+                        source={facebook}/>
+                    <Text style={[styles.textGenreal, styles.textFacebook]}>Sign up with Facebook</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.signupScreenButton}
+                    onPress={SignUpPage._onPressButton}
+                    underlayColor='#fff'>
+                    <Text style={styles.signupText}>Already have an account? Log In</Text>
+                </TouchableOpacity>
+
+            </View>
+        );
+    }
+}
+
+
+export default SignUpPage;
