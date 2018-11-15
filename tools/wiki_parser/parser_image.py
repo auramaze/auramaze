32fc6c5b72ad119b33d6d58f1b561fbe0d0290c4
Tim <xyshen@umich.edu>
2018-11-14 23:55:17 -0500

    add cropping to face detection output

--- tools/wiki_parser/parser_image.py
+++ tools/wiki_parser/parser_image.py
@@ -6,6 +6,8 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm as tqdm
 
+URL = 'https://en.wikipedia.org'
+
 class WikiImageParser:
 
     def __init__(self):
@@ -23,8 +25,21 @@ class WikiImageParser:
             link = ''
         self.dict[user_name] = link
 
-    def _download(self, user_name, link):
-
+    def download(self, user_name, link, folder):
+        try:
+            url = URL + link
+            html = requests.get(url).text
+            soup = BeautifulSoup(html, 'html.parser')
+            div = soup.find('div', class_='fullImageLink')
+            if div is not None:
+                img_url = 'https:' + div.a.get('href')
+                img = requests.get(img_url)
+                suffix = img_url.split('.')[-1]
+                file_name = user_name + '.' + suffix
+                with open(os.path.join(folder, file_name), 'wb') as f:
+                    f.write(img.content)
+        except AttributeError:
+            return
 
     def save(self, output_path):
         with open(output_path, 'w') as f:
@@ -37,6 +52,7 @@ class WikiImageParser:
             text = f.read()
         self._parse(user_name, text)
 
+
 def run(input_path, output_path):
     driver = WikiImageParser()
 
@@ -45,6 +61,7 @@ def run(input_path, output_path):
         driver.process(input)
 
     driver.save(output_path)
+
 
 def main():
     parser = argparse.ArgumentParser(description='wikipedia parser')
@@ -55,6 +72,10 @@ def main():
     run(args.input_path, args.output_path)
 
 
-
 if __name__ == '__main__':
-    main()
+    driver = WikiImageParser()
+    with open('test.json') as f:
+        dic = json.load(f)
+    for user_name, link in tqdm(sorted(dic.items())[1590:]):
+        driver.download(user_name, link, 'img')
+
