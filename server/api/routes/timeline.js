7cca4c61a4d4e00e622ce54055eaa6ad238e4d27
Zian Ke <kezian@umich.edu>
2018-11-15 16:34:12 -0500

    Fetch author info

--- server/api/routes/timeline.js
+++ server/api/routes/timeline.js
@@ -17,7 +17,7 @@ router.get('/', [
 
     const {payload: {id}} = req;
 
-    rds.query('SELECT text.*, art.id AS art_id, art.username AS art_username, art.title AS art_title, art.image AS art_image, artizen.id AS artizen_id, artizen.username AS artizen_username, artizen.name AS artizen_name, artizen.avatar AS artizen_avatar FROM text LEFT JOIN art ON text.art_id=art.id LEFT JOIN artizen ON text.artizen_id=artizen.id INNER JOIN follow ON text.author_id=follow.followee_id WHERE follow.follower_id=? ORDER BY text.id DESC LIMIT 10', [id], (err, result, fields) => {
+    rds.query('SELECT text.*, author.username AS author_username, author.name AS author_name, author.avatar AS author_avatar, art.username AS art_username, art.title AS art_title, art.image AS art_image, artizen.username AS artizen_username, artizen.name AS artizen_name, artizen.avatar AS artizen_avatar FROM text INNER JOIN artizen AS author ON text.author_id=author.id LEFT JOIN art ON text.art_id=art.id LEFT JOIN artizen AS artizen ON text.artizen_id=artizen.id INNER JOIN follow ON text.author_id=follow.followee_id WHERE follow.follower_id=? ORDER BY text.id DESC LIMIT 10', [id], (err, result, fields) => {
         /* istanbul ignore if */
         if (err) {
             next(err);
