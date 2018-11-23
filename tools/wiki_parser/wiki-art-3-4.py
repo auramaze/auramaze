261864ac0f2a2e7a16f7d3f587893e6f8351ef34
Zian Ke <kezian@umich.edu>
2018-11-23 17:04:36 -0500

    Manual filter for first 10k

---
+++ tools/wiki_parser/wiki-art-3-4.py
@@ -0,0 +1,22 @@
+'''
+Add manual filter results to art json
+'''
+import json
+
+prev = 3
+next = 4
+index = 0
+
+fault = json.load(open('wiki-art_{}-{}.4.json'.format(index, prev)))
+status = json.load(open('wiki-art_{}-{}.5.json'.format(index, prev)))
+arts = json.load(open('wiki-art_{}-{}.json'.format(index, prev)))
+
+for art in arts:
+    username = art['username']
+    if username in status:
+        art['wikipedia_match'] = status[username]
+    if username in fault:
+        art['wikipedia_url'] = fault[username]
+        art['wikipedia_match'] = 1
+
+json.dump(arts, open('wiki-art_{}-{}.json'.format(index, next), 'w+'), indent=4)
--- tools/wiki_parser/wiki-art-3-4/index.js
+++ tools/wiki_parser/wiki-art-3-4/index.js
@@ -1,3 +1,46 @@
+function satisfy_variants(art) {
+    return art.wikipedia_url.includes('en.wikipedia.org') && art.levenshtein_distance > 70;
+}
+
 $(document).ready(function () {
     console.log("ready!");
+    $.getJSON("http://127.0.0.1:3000/wiki-art_0-3.json", function (data) {
+        var items = [];
+        const status = {};
+
+        $.each(data, function (key, val) {
+            status[val.username] = 0;
+            if (satisfy_variants(val)) {
+                items.push(`
+                    <tr>
+                        <td width="10%">
+                            ${val.title}
+                        </td>
+                        <td width="20%">
+                            <img src="${val.image_url}">
+                        </td>
+                        <td width="60%">
+                            <iframe width="100%" height="100%" src="${val.wikipedia_url}"></iframe>
+                        </td>
+                        <td width="10%">
+                            <input username="${val.username}" type="radio" name="${val.username}" value="0" checked> 0<br>
+                            <input username="${val.username}" type="radio" name="${val.username}" value="1"> 1<br>
+                            <input username="${val.username}" type="radio" name="${val.username}" value="2"> 2
+                        </td>
+                    </tr>
+                `);
+            }
+        });
+
+        $("<table/>", {
+            html: items.join("")
+        }).appendTo("body");
+
+
+        $('input[type=radio]').change(function () {
+            status[$(this).attr('username')] = parseInt(this.value);
+            $('#result').html(JSON.stringify(status));
+            console.log(status);
+        });
+    });
 });
