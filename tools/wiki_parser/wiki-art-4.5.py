ca494e3d16766e9f50e57fd27ad6e0923a03a891
Zian Ke <kezian@umich.edu>
2018-11-23 21:37:04 -0500

    Download art wikipedia html

---
+++ tools/wiki_parser/wiki-art-4.5.py
@@ -0,0 +1,22 @@
+'''
+Filter wikipedia url with status 1 or 2
+'''
+import json
+import requests
+
+prev = 4
+next = 5
+index = 1
+
+arts = json.load(open('wiki-art_{}-{}.json'.format(index, prev)))
+
+for art in arts:
+    username = art['username']
+    if 'wikipedia_url' in art and art['wikipedia_url']:
+        wikipedia_url = art['wikipedia_url']
+        print(wikipedia_url)
+        r = requests.get(wikipedia_url)
+        if r.status_code == 200:
+            html_doc = r.text
+            with open('art-html/{}.html'.format(username), 'w+') as f:
+                f.write(html_doc)
