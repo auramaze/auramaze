36c314bee39869bf0352c7b0930d062e7a5a2369
Zian Ke <kezian@umich.edu>
2018-11-23 21:32:41 -0500

    Remove unmatched wikipedia url

--- tools/wiki_parser/wiki-art-3-4.py
+++ tools/wiki_parser/wiki-art-3-4.py
@@ -1,5 +1,5 @@
 '''
-Add manual filter results to art json
+Filter wikipedia url with status 1 or 2
 '''
 import json
 
@@ -16,10 +16,13 @@ arts = json.load(open('wiki-art_{}-{}.json'.format(index, prev)))
 
 for art in arts:
     username = art['username']
+    match = 0
     if username in status:
-        art['wikipedia_match'] = status[username]
+        match = status[username]
     if username in fault:
         art['wikipedia_url'] = fault[username]
-        art['wikipedia_match'] = 1
+        match = 1
+    if match == 0:
+        art['wikipedia_url'] = None
 
 json.dump(arts, open('wiki-art_{}-{}.json'.format(index, next), 'w+'), indent=4)
