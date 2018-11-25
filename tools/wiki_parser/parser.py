a3b0a30d0e980c5fba815e9a5b969dad07556472
Tim <xyshen@umich.edu>
2018-11-25 01:10:35 -0500

    change logic of wiki parser to include more text if necessary

--- tools/wiki_parser/parser.py
+++ tools/wiki_parser/parser.py
@@ -11,51 +11,79 @@ class WikiParser:
         self.dict = {}
         self.lang = lang
 
+    def _find_header(soup):
+        for section in soup.find_all('h2'):
+            try:
+                count = self.dict.get(section.span.text, 0)
+                self.dict[section.span.text] = count + 1
+            except:
+                continue
+
     def _parse(self, user_name, text):
-        wiki_dic = {}
+        wiki_doc = {}
         soup = BeautifulSoup(text, 'html.parser')
 
-        for section in soup.find_all('h2'):
-            print(section.span.text)
-        return 
-
-
-        for table in soup.find_all("table"):
+        for table in soup.find_all('table'):
             table.decompose()
-        text = str(soup)
 
-        start_title = text.find("<title")
-        end_start_title = text.find(">", start_title+1)
+        title = soup.find('title').get_text()
         if self.lang == 'CN':
-            stop_title = text.find(" - 维基百科", end_start_title + 1)
+            stop_title = title.find(' - 维基百科')
         else:
-            stop_title = text.find(" - Wikipedia", end_start_title + 1)
-        title = text[end_start_title + 1 : stop_title]
-        wiki_dic["title"] = title
+            stop_title = title.find(' - Wikipedia')
+        wiki_doc['title'] = title[:stop_title]
 
-        start_introduction = text.find("<p>")
-        stop_introduction = text.find('<div id="toctitle">', start_introduction + 1)
-        if '<div id="toctitle">' not in text:
-            stop_introduction_1 = text.find('<div id="toc"')
-            stop_introduction_2 = text.find('<h2>')
-            if stop_introduction_1 == -1:
-                stop_introduction = stop_introduction_2
+        wiki_doc['html'] = []
+
+        text = str(soup)
+        list = text.split('<h2>')
+        for i in range(len(list)):
+            if i != 0:
+                segment = '<h2>' + list[i]
             else:
-                stop_introduction = min(stop_introduction_1, stop_introduction_2)
+                header = 'DEFAULT'
+                segment = list[i]
 
-        introduction = text[start_introduction : stop_introduction - 1]
-        soup = BeautifulSoup(introduction, 'html.parser')
-        for sup in soup.find_all('sup'):
-            sup.decompose()
-        for link in soup.find_all('a'):
-            link.unwrap()
-        for img in soup.find_all('img'):
-            img.decompose()
-        introduction = ''.join([str(para) for para in soup.find_all('p')])
+            para_soup = BeautifulSoup(segment, 'html.parser')
+            if i != 0:
+                try:
+                    header = para_soup.find('h2').find('span', class_='mw-headline').get_text()
+                except:
+                    continue
+            for sup in para_soup.find_all('sup'):
+                sup.decompose()
+            for link in para_soup.find_all('a'):
+                link.unwrap()
+            for img in para_soup.find_all('img'):
+                img.decompose()
+            content = ''.join([str(para) for para in para_soup.find_all('p')])
 
-        wiki_dic["html"] = introduction
+            if content != '':
+                wiki_doc['html'].append({'header': header, 'content': content})
 
-        self.dict[user_name] = wiki_dic
+        # start_introduction = text.find("<p>")
+        # stop_introduction = text.find('<div id="toctitle">', start_introduction + 1)
+        # if '<div id="toctitle">' not in text:
+        #     stop_introduction_1 = text.find('<div id="toc"')
+        #     stop_introduction_2 = text.find('<h2>')
+        #     if stop_introduction_1 == -1:
+        #         stop_introduction = stop_introduction_2
+        #     else:
+        #         stop_introduction = min(stop_introduction_1, stop_introduction_2)
+        #
+        # introduction = text[start_introduction : stop_introduction - 1]
+        # soup = BeautifulSoup(introduction, 'html.parser')
+        # for sup in soup.find_all('sup'):
+        #     sup.decompose()
+        # for link in soup.find_all('a'):
+        #     link.unwrap()
+        # for img in soup.find_all('img'):
+        #     img.decompose()
+        # introduction = ''.join([str(para) for para in soup.find_all('p')])
+        #
+        # wiki_doc["html"] = introduction
+
+        self.dict[user_name] = wiki_doc
 
     def save(self, output_path):
         with open(output_path, 'w') as f:
@@ -85,7 +113,7 @@ def main():
             html_path = os.path.join(input_path, os.path.basename(file))
             driver.process(html_path)
 
-        # driver.save(output_path)
+        driver.save(output_path)
 
 
 if __name__ == '__main__':
