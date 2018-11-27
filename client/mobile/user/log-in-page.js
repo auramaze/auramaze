ad86c7a8bc2c384735f90037e31ce98fe04609d9
Zian Ke <kezian@umich.edu>
2018-11-27 12:41:28 -0500

    Get Facebook avatar

--- client/mobile/user/log-in-page.js
+++ client/mobile/user/log-in-page.js
@@ -83,11 +83,11 @@ class LogInPage extends React.Component {
                 permissions,
                 declinedPermissions,
             } = await Expo.Facebook.logInWithReadPermissionsAsync(config.expo.facebookAppId, {
-                permissions: ['public_profile'],
+                permissions: ['public_profile']
             });
             if (type === 'success') {
                 // Get the user's name using Facebook's Graph API
-                const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+                const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=id,name,picture.width(250)`);
                 const profile = await response.json();
                 alert(`Hi ${profile.name}!`);
                 console.log(profile);
