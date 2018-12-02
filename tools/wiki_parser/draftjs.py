b837358ca747c2343b237fa9240922ca78eb0328
Tim <xyshen@umich.edu>
2018-12-01 21:38:47 -0500

    fix bug in draftjs

--- tools/wiki_parser/draftjs.py
+++ tools/wiki_parser/draftjs.py
@@ -1,37 +1,27 @@
 import json
 import requests
+from tqdm import tqdm
+import time
 
 if __name__ == '__main__':
     for group in ['artist', 'genre', 'style', 'museum']:
         artizens = json.load(open('wiki-json/wiki-{}-2.json'.format(group)))
-        for artizen in artizens:
-            if artizen['wikipedia']:
-                html = artizen['wikipedia']['html']
-                if html:
-                    payload = {'html': html}
-                    r = requests.post('http://localhost:8000/html-to-content', json=payload)
-                    try:
-                        artizen['wikipedia']['content'] = r.json()
-                    except:
-                        print(json.dumps(payload))
-                        print(html)
-                        exit(0)
-                else:
-                    artizen['wikipedia']['content'] = None
-
-            if artizen['wikipedia_zh']:
-                html = artizen['wikipedia_zh']['html']
-                if html:
-                    payload = {'html': html}
-                    r = requests.post('http://localhost:8000/html-to-content', json=payload)
-                    try:
-                        artizen['wikipedia']['content'] = r.json()
-                    except:
-                        print(json.dumps(payload))
-                        print(html)
-                        exit(0)
-                else:
-                    artizen['wikipedia']['content'] = None
+        for artizen in tqdm(artizens):
+            time.sleep(0.05)
+            for key_type in ['wikipedia', 'wikipedia_zh']:
+                if artizen[key_type]:
+                    html = artizen[key_type]['html']
+                    if html:
+                        payload = {'html': html}
+                        r = requests.post('http://localhost:8000/html-to-content', json=payload)
+                        try:
+                            artizen[key_type]['content'] = r.json()
+                        except Exception as e:
+                            print(json.dumps(payload))
+                            print(html)
+                            raise e
+                    else:
+                        artizen[key_type]['content'] = None
 
         with open('wiki-json/wiki-{}-3.json'.format(group), 'w', encoding='utf8') as f:
             json.dump(artizens, f, indent=4, ensure_ascii=False)
