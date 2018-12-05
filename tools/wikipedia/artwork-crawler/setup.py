63f0bddcd80a6d58e85530e92cbcd151956f2ff6
Ray Cao <youdymoo@umich.edu>
2018-12-04 22:04:09 -0500

    setup

---
+++ tools/wikipedia/artwork-crawler/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='artwork-crawler',
+    version='0.1',
+    description='Web Crawler for Artworks of EECS 441',
+    url='http://github.com/youdymoo/artwork-crawler',
+    author='Ray Cao',
+    author_email='youdymoo@umich.edu',
+    include_package_data=True,
+    install_requires=[
+        'google-api-python-client',
+        'Pillow',
+        'numpy',
+        'scipy',
+        'fastdtw',
+        'unidecode'
+    ],
+    license='MIT',
+    packages=['artwork-crawler'],
+    zip_safe=False
+)
