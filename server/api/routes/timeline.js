97e1bf0ddd90b489cc024991fe9f9e0023e3b3d9
Zian Ke <kezian@umich.edu>
2018-12-07 11:03:53 -0500

    Temp fix bug of timeline

--- server/api/routes/timeline.js
+++ server/api/routes/timeline.js
@@ -23,19 +23,33 @@ router.get('/', [
     const {payload: {id}} = req;
 
     if (parseInt(req.query.min) >= 0) {
-        const min = parseInt(req.query.min);
+        // const min = parseInt(req.query.min);
+        //
+        // rds.query('SELECT SQL_CALC_FOUND_ROWS text.*, author.id AS author_id, author.username AS author_username, author.name AS author_name, author.avatar AS author_avatar, art.username AS art_username, art.title AS art_title, art.image AS art_image, artizen.username AS artizen_username, artizen.name AS artizen_name, artizen.avatar AS artizen_avatar, SUM(CASE WHEN status=1 THEN 1 ELSE 0 END) AS up, SUM(CASE WHEN status=-1 THEN 1 ELSE 0 END) AS down, (SELECT vote.status FROM vote WHERE vote.text_id=text.id AND vote.user_id=?) AS status FROM text INNER JOIN artizen AS author ON text.user_id=author.id LEFT JOIN art ON text.art_id=art.id LEFT JOIN artizen AS artizen ON text.artizen_id=artizen.id INNER JOIN follow ON text.user_id=follow.artizen_id LEFT JOIN vote ON text.id=vote.text_id WHERE follow.user_id=? AND text.valid=1 AND text.id>=? GROUP BY text.id ORDER BY text.id DESC LIMIT 10; SELECT FOUND_ROWS() AS total;', [id, id, min], (err, result, fields) => {
+        //     /* istanbul ignore if */
+        //     if (err) {
+        //         next(err);
+        //     } else {
+        //         const response = {data: result[0], next: null};
+        //
+        //         if (result[0].length && result[0][result[0].length - 1].id > min) {
+        //             const nextMax = result[0][result[0].length - 1].id - 1;
+        //             response.next = `${process.env.API_ENDPOINT}/timeline?max=${nextMax}`;
+        //         }
+        //         res.json(response);
+        //     }
+        // });
 
-        rds.query('SELECT SQL_CALC_FOUND_ROWS text.*, author.id AS author_id, author.username AS author_username, author.name AS author_name, author.avatar AS author_avatar, art.username AS art_username, art.title AS art_title, art.image AS art_image, artizen.username AS artizen_username, artizen.name AS artizen_name, artizen.avatar AS artizen_avatar, SUM(CASE WHEN status=1 THEN 1 ELSE 0 END) AS up, SUM(CASE WHEN status=-1 THEN 1 ELSE 0 END) AS down, (SELECT vote.status FROM vote WHERE vote.text_id=text.id AND vote.user_id=?) AS status FROM text INNER JOIN artizen AS author ON text.user_id=author.id LEFT JOIN art ON text.art_id=art.id LEFT JOIN artizen AS artizen ON text.artizen_id=artizen.id INNER JOIN follow ON text.user_id=follow.artizen_id LEFT JOIN vote ON text.id=vote.text_id WHERE follow.user_id=? AND text.valid=1 AND text.id>=? GROUP BY text.id ORDER BY text.id DESC LIMIT 10; SELECT FOUND_ROWS() AS total;', [id, id, min], (err, result, fields) => {
+        const max = Number.MAX_SAFE_INTEGER;
+
+        rds.query('SELECT SQL_CALC_FOUND_ROWS text.*, author.id AS author_id, author.username AS author_username, author.name AS author_name, author.avatar AS author_avatar, art.username AS art_username, art.title AS art_title, art.image AS art_image, artizen.username AS artizen_username, artizen.name AS artizen_name, artizen.avatar AS artizen_avatar, SUM(CASE WHEN status=1 THEN 1 ELSE 0 END) AS up, SUM(CASE WHEN status=-1 THEN 1 ELSE 0 END) AS down, (SELECT vote.status FROM vote WHERE vote.text_id=text.id AND vote.user_id=?) AS status FROM text INNER JOIN artizen AS author ON text.user_id=author.id LEFT JOIN art ON text.art_id=art.id LEFT JOIN artizen AS artizen ON text.artizen_id=artizen.id INNER JOIN follow ON text.user_id=follow.artizen_id LEFT JOIN vote ON text.id=vote.text_id WHERE follow.user_id=? AND text.valid=1 AND text.id<=? GROUP BY text.id ORDER BY text.id DESC LIMIT 10; SELECT FOUND_ROWS() AS total;', [id, id, max], (err, result, fields) => {
             /* istanbul ignore if */
             if (err) {
                 next(err);
             } else {
                 const response = {data: result[0], next: null};
-
-                if (result[0].length && result[0][result[0].length - 1].id > min) {
-                    const nextMax = result[0][result[0].length - 1].id - 1;
-                    response.next = `${process.env.API_ENDPOINT}/timeline?max=${nextMax}`;
-                }
+                const nextMax = result[0][result[0].length - 1].id - 1;
+                response.next = `${process.env.API_ENDPOINT}/timeline?max=${nextMax}`;
                 res.json(response);
             }
         });
