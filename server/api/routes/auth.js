cdeadaf07c772641d47c173e779234959e1b624e
Zian Ke <kezian@umich.edu>
2018-12-12 13:54:36 -0500

    Remove unused code

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -4,43 +4,18 @@ const router = express.Router();
 const passport = require('passport');
 const {body, oneOf, validationResult} = require('express-validator/check');
 const authController = require('./auth.controller');
-const authMobile = require('./auth.mobile');
-const {auth} = require('./auth.config');
 const common = require('./common');
 const rds = common.rds;
 const _ = require('lodash');
-// Setting up the passport middleware for each of the OAuth providers
-const googleAuth = passport.authenticate('google', {scope: ['profile']});
-const facebookAuth = passport.authenticate('facebook');
 
-// Routes that are triggered by the callbacks from each OAuth provider once 
-// the user has authenticated successfully
-router.get('/google/callback', googleAuth, authController.google);
-router.get('/facebook/callback', facebookAuth, authController.facebook);
-
-// This custom middleware allows us to attach the socket id to the session
-// With that socket id we can send back the right user info to the right 
-// socket
-router.use((req, res, next) => {
-    req.session.socketId = req.query.socketId;
-    next();
-});
-
-// Routes that are triggered on the web client
-router.get('/google', googleAuth);
-router.get('/facebook', facebookAuth);
-
-// Routes that are triggered on the mobile client
-router.post('/google', [body('id').isInt()], authMobile.google);
-router.post('/facebook', [body('id').isInt()], authMobile.facebook);
-router.post('/google/mobile', [body('id').isInt()], authMobile.google);
-router.post('/facebook/mobile', [body('id').isInt()], authMobile.facebook);
+router.post('/google', [body('id').isInt()], authController.google);
+router.post('/facebook', [body('id').isInt()], authController.facebook);
 
 // Sign up with email
 router.post('/signup', [
     body('email').isEmail(),
     body('password').custom(common.validatePassword).withMessage('Invalid password')
-], auth.optional, (req, res, next) => {
+], (req, res, next) => {
     const errors = validationResult(req);
     if (!validationResult(req).isEmpty()) {
         return res.status(400).json({errors: errors.array()});
@@ -78,7 +53,7 @@ router.post('/login', [
         body('id').custom(common.validateUsername).withMessage('Invalid username')
     ]),
     body('password').custom(common.validatePassword).withMessage('Invalid password')
-], auth.optional, (req, res, next) => {
+], (req, res, next) => {
     const errors = validationResult(req);
     if (!validationResult(req).isEmpty()) {
         return res.status(400).json({errors: errors.array()});
@@ -99,20 +74,6 @@ router.post('/login', [
             }
         }
     })(req, res, next);
-});
-
-// Test route only authenticated users have access
-router.get('/current', auth.required, (req, res, next) => {
-    const {payload: {id}} = req;
-
-    rds.query('SELECT * FROM artizen WHERE id=?', [id], (err, result, fields) => {
-        /* istanbul ignore else */
-        if (!err && result[0]) {
-            return res.json(result[0]);
-        } else {
-            return res.status(400);
-        }
-    });
 });
 
 module.exports = router;
