e82cf18700d3e875fb407b2f3b861c15173aa56e
Zian Ke <kezian@umich.edu>
2018-12-12 15:17:57 -0500

    Add constraints to auth api

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -3,13 +3,147 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const {body, oneOf, validationResult} = require('express-validator/check');
-const authController = require('./auth.controller');
 const common = require('./common');
 const rds = common.rds;
 const _ = require('lodash');
+const request = require('request');
 
-router.post('/google', [body('id').isInt()], authController.google);
-router.post('/facebook', [body('id').isInt()], authController.facebook);
+router.post('/google', oneOf([
+    body('access_token').exists(),
+    body('id').isInt()
+]), (req, res, next) => {
+    const errors = validationResult(req);
+    if (!validationResult(req).isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    let {id, name, avatar, access_token} = req.body;
+    if (access_token) {
+        request.post({
+            url: 'https://www.googleapis.com/userinfo/v2/me',
+            headers: {Authorization: `Bearer ${access_token}`},
+            json: true
+        }, (error, response, body) => {
+            if (response && response.statusCode === 200) {
+                id = body.id;
+                name = body.name;
+                avatar = body.picture;
+                rds.query('INSERT INTO artizen (google, name, avatar) VALUES (?) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)',
+                    [[id, JSON.stringify({default: name}), avatar]],
+                    (err, result, fields) => {
+                        /* istanbul ignore if */
+                        if (err) {
+                            next(err);
+                        } else {
+                            rds.query('SELECT id, username from artizen where id=LAST_INSERT_ID()', (err, result, fields) => {
+                                /* istanbul ignore if */
+                                if (err) {
+                                    next(err);
+                                } else {
+                                    const {id, username} = result[0];
+                                    const user = common.toAuthJSON({id, username});
+                                    res.json(user);
+                                }
+                            });
+                        }
+                    });
+            } else {
+                res.status(400).json({
+                    code: 'GOOGLE_ACCESS_TOKEN_INVALID',
+                    message: `Google access token invalid: ${req.body.access_token}`
+                });
+            }
+        });
+    } else {
+        rds.query('INSERT INTO artizen (google, name, avatar) VALUES (?) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)',
+            [[id, JSON.stringify({default: name}), avatar]],
+            (err, result, fields) => {
+                /* istanbul ignore if */
+                if (err) {
+                    next(err);
+                } else {
+                    rds.query('SELECT id, username from artizen where id=LAST_INSERT_ID()', (err, result, fields) => {
+                        /* istanbul ignore if */
+                        if (err) {
+                            next(err);
+                        } else {
+                            const {id, username} = result[0];
+                            const user = common.toAuthJSON({id, username});
+                            res.json(user);
+                        }
+                    });
+                }
+            });
+    }
+});
+
+router.post('/facebook', oneOf([
+    body('access_token').exists(),
+    body('id').isInt()
+]), (req, res, next) => {
+    const errors = validationResult(req);
+    if (!validationResult(req).isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    let {id, name, avatar, access_token} = req.body;
+    if (access_token) {
+        request.get({
+            url: `https://graph.facebook.com/me?access_token=${access_token}&fields=id,name,picture.width(250)`,
+            json: true
+        }, (error, response, body) => {
+            if (response && response.statusCode === 200) {
+                id = body.id;
+                name = body.name;
+                avatar = body.picture && body.picture.data && body.picture.data.url;
+                rds.query('INSERT INTO artizen (facebook, name, avatar) VALUES (?) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)',
+                    [[id, JSON.stringify({default: name}), avatar]],
+                    (err, result, fields) => {
+                        /* istanbul ignore if */
+                        if (err) {
+                            next(err);
+                        } else {
+                            rds.query('SELECT id, username from artizen where id=LAST_INSERT_ID()', (err, result, fields) => {
+                                /* istanbul ignore if */
+                                if (err) {
+                                    next(err);
+                                } else {
+                                    const {id, username} = result[0];
+                                    const user = common.toAuthJSON({id, username});
+                                    res.json(user);
+                                }
+                            });
+                        }
+                    });
+            } else {
+                res.status(400).json({
+                    code: 'FACEBOOK_ACCESS_TOKEN_INVALID',
+                    message: `Facebook access token invalid: ${req.body.access_token}`
+                });
+            }
+        });
+    } else {
+        rds.query('INSERT INTO artizen (facebook, name, avatar) VALUES (?) ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)',
+            [[id, JSON.stringify({default: name}), avatar]],
+            (err, result, fields) => {
+                /* istanbul ignore if */
+                if (err) {
+                    next(err);
+                } else {
+                    rds.query('SELECT id, username from artizen where id=LAST_INSERT_ID()', (err, result, fields) => {
+                        /* istanbul ignore if */
+                        if (err) {
+                            next(err);
+                        } else {
+                            const {id, username} = result[0];
+                            const user = common.toAuthJSON({id, username});
+                            res.json(user);
+                        }
+                    });
+                }
+            });
+    }
+});
 
 // Sign up with email
 router.post('/signup', [
